31a349ad90b8a71ad8a4638eeac5b22873028e34
Donovan HOANG <dhoang@ippon.fr>
2024-08-01 21:53:38 +0200

    feat: catch command arg

--- src/main.go
+++ src/main.go
@@ -3,19 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
-var asciiart string = `
-___                      __           
-/ _ \_ __ ___  ___ _ __  / _|_ __ __ _ 
-/ /_\/ '__/ _ \/ _ \ '_ \| |_| '__/ _` + "`" + ` | 
+var (
+	asciiart string = `
+   ___                      __           
+  / _ \_ __ ___  ___ _ __  / _|_ __ __ _ 
+ / /_\/ '__/ _ \/ _ \ '_ \| |_| '__/ _` + "`" + ` | 
 / /_\\| | |  __/  __/ | | |  _| | | (_| |
-\____/|_|  \___|\___|_| |_|_| |_|  \__,_|`
+\____/|_|  \___|\___|_| |_|_| |_|  \__,_|
+                                         `
+	command string
+)
 
 func init() {
 	flag.Parse()
+	if len(os.Args) > 1 {
+		command = os.Args[1]
+	}
 }
 
 func main() {
-	fmt.Printf(asciiart)
+	fmt.Println(asciiart)
+
+	switch command {
+	case "help":
+		fmt.Println("No command for now.")
+	default:
+		if command != "" {
+			fmt.Println("Unknown command:", command)
+		}
+		fmt.Println("Use 'go run main.go help' for usage information.")
+	}
 }
