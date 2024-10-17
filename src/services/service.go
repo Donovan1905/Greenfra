373a80368d9892f6a6fe9bc281ef5719252e15d8
Donovan HOANG <dhoang@ippon.fr>
2024-10-17 21:53:14 +0200

    chore: move constant and regional energy mix to root service

--- src/services/service.go
+++ src/services/service.go
@@ -1,5 +1,7 @@
 package services
 
+import "fmt"
+
 type ResourceChange struct {
 	Address string                 `json:"address"`
 	Mode    string                 `json:"mode"`
@@ -10,4 +12,27 @@ type ResourceChange struct {
 
 type Service interface {
 	Analyze(changes []ResourceChange) error
+}
+
+const powerConsumptionPerVCPU = 2.10           // Wh per vCPU
+const powerConsumptionPerMBofMemory = 0.000384 // Wh per MB of memory
+
+var carbonCosts = map[string]float64{ // gCO2eq per KWh
+	"eu-west-3":    20,
+	"eu-central-1": 200,
+}
+
+func calculateMonthlyPowerConsumption(vCPUs int, memory int) float64 {
+	hoursPerDay := 24.0
+	daysPerMonth := 30.0
+	return (float64(vCPUs) * powerConsumptionPerVCPU * hoursPerDay * daysPerMonth) + (float64(memory) * powerConsumptionPerMBofMemory * hoursPerDay * daysPerMonth)
+}
+
+func calculateCarbonFootprint(power float64, region string) float64 {
+	regionalCarbonCost, ok := carbonCosts[region]
+	if !ok {
+		fmt.Println("Unknown region. Using default carbon cost.")
+	}
+
+	return power * regionalCarbonCost
 }
