b787abbe83dd5856736099a2598970c82f9f2dba
Donovan Hoang <44325654+Donovan1905@users.noreply.github.com>
2024-10-18 19:50:47 +0200

    feat: add support for lambda footprint estimation (#9)
    * chore: move constant and regional energy mix to root service
    
    * feat: estimate lambda footprint for one month of usage
    
    * refactor: energy consumption function takes duration as parameter
    
    * feat: estimate lambda footprint for a mean execution duration and number

--- src/services/ec2.go
+++ src/services/ec2.go
@@ -16,14 +16,6 @@ type EC2Service struct {
 	client *ec2.Client
 }
 
-const powerConsumptionPerVCPU = 2.10           // Wh per vCPU
-const powerConsumptionPerMBofMemory = 0.000384 // Wh per MB of memory
-
-var carbonCosts = map[string]float64{ // gCO2eq per KWh
-	"eu-west-3":    20,
-	"eu-central-1": 200,
-}
-
 func NewEC2Service(cfg aws.Config) *EC2Service {
 	return &EC2Service{
 		client: ec2.NewFromConfig(cfg),
@@ -84,7 +76,7 @@ func (s *EC2Service) printInstanceSpecs(instanceTypes []string, region string) {
 		memory := *instanceType.MemoryInfo.SizeInMiB
 
 		// Calculate monthly power consumption
-		powerConsumption := calculateMonthlyPowerConsumption(int(vcpus), int(memory)) / 1000
+		powerConsumption := calculateMonthlyPowerConsumption(float64(vcpus), int(memory), hoursInMonth) / 1000
 
 		// Calculate monthly carbon impact
 		carbonImpact := calculateCarbonFootprint(powerConsumption, region)
@@ -101,19 +93,4 @@ func (s *EC2Service) printInstanceSpecs(instanceTypes []string, region string) {
 
 	// Render the table
 	table.Render()
-}
-
-func calculateMonthlyPowerConsumption(vCPUs int, memory int) float64 {
-	hoursPerDay := 24.0
-	daysPerMonth := 30.0
-	return (float64(vCPUs) * powerConsumptionPerVCPU * hoursPerDay * daysPerMonth) + (float64(memory) * powerConsumptionPerMBofMemory * hoursPerDay * daysPerMonth)
-}
-
-func calculateCarbonFootprint(power float64, region string) float64 {
-	regionalCarbonCost, ok := carbonCosts[region]
-	if !ok {
-		fmt.Println("Unknown region. Using default carbon cost.")
-	}
-
-	return power * regionalCarbonCost
 }
