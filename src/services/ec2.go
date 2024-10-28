ce3527d74648c72d0366d5ec65caf637815a3d76
Donovan HOANG <dhoang@ippon.fr>
2024-10-28 19:33:54 +0100

    feat: add ec2 terraform resource reference to table

--- src/services/ec2.go
+++ src/services/ec2.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	greenfraTypes "greenfra/src/types"
 	"log"
 	"os"
 
@@ -22,35 +23,40 @@ func NewEC2Service(cfg aws.Config) *EC2Service {
 	}
 }
 
-func (s *EC2Service) Analyze(changes []ResourceChange, region string) error {
-	instanceTypes := make([]string, 0)
+func (s *EC2Service) Analyze(changes []ResourceChange, region string, comments map[string]greenfraTypes.ResourceMetadata) error {
+	instanceSpecs := make(map[string][]greenfraTypes.ResourceMetadata) // Map to hold instance types and their references
 
 	for _, change := range changes {
 		if change.Type == "aws_instance" {
-
 			if after, ok := change.Change["after"].(map[string]interface{}); ok {
 				instanceType, ok := after["instance_type"].(string)
 				if !ok {
 					log.Printf("Warning: instance_type is not a string or is missing in change: %v", change.Change)
 					continue
 				}
-				instanceTypes = append(instanceTypes, instanceType)
+				// Capture the resource reference and add it to the map
+				resourceMetadata := greenfraTypes.ResourceMetadata{
+					ResourceReference: change.Address,
+					// You can initialize Metadata as needed
+				}
+				instanceSpecs[instanceType] = append(instanceSpecs[instanceType], resourceMetadata)
 			} else {
 				log.Printf("Warning: 'after' map is missing in change: %v", change.Change)
 			}
 		}
 	}
 
-	if len(instanceTypes) > 0 {
-		s.printInstanceSpecs(instanceTypes, region)
+	if len(instanceSpecs) > 0 {
+		s.printInstanceSpecs(instanceSpecs, region)
 	}
 	return nil
 }
 
-func (s *EC2Service) printInstanceSpecs(instanceTypes []string, region string) {
-	awsInstanceTypes := make([]types.InstanceType, len(instanceTypes))
-	for i, instanceType := range instanceTypes {
-		awsInstanceTypes[i] = types.InstanceType(instanceType)
+func (s *EC2Service) printInstanceSpecs(instanceSpecs map[string][]greenfraTypes.ResourceMetadata, region string) {
+	awsInstanceTypes := make([]types.InstanceType, 0)
+
+	for instanceType := range instanceSpecs {
+		awsInstanceTypes = append(awsInstanceTypes, types.InstanceType(instanceType))
 	}
 
 	input := &ec2.DescribeInstanceTypesInput{
@@ -63,8 +69,15 @@ func (s *EC2Service) printInstanceSpecs(instanceTypes []string, region string) {
 	}
 	fmt.Print("\n")
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Instance Type", "vCPUs", "Memory (MiB)", "Estimated Monthly Power Consumption (kWh)", "Carbon impact (gCO2eq)"})
-	table.SetHeaderColor(tablewriter.Colors{tablewriter.FgHiGreenColor}, tablewriter.Colors{tablewriter.FgHiGreenColor}, tablewriter.Colors{tablewriter.FgHiGreenColor}, tablewriter.Colors{tablewriter.FgHiGreenColor}, tablewriter.Colors{tablewriter.FgHiGreenColor})
+	headers := []string{"Instance Type", "Resource References", "vCPUs", "Memory (MiB)", "Estimated Monthly Power Consumption (kWh)", "Carbon impact (gCO2eq)"}
+	table.SetHeader(headers)
+
+	colors := make([]tablewriter.Colors, len(headers))
+	for i := range colors {
+		colors[i] = tablewriter.Colors{tablewriter.FgHiGreenColor}
+	}
+	table.SetHeaderColor(colors...)
+
 	table.SetRowLine(true)
 	table.SetAlignment(tablewriter.ALIGN_LEFT)
 
@@ -73,11 +86,20 @@ func (s *EC2Service) printInstanceSpecs(instanceTypes []string, region string) {
 		memory := *instanceType.MemoryInfo.SizeInMiB
 
 		powerConsumption := calculateMonthlyPowerConsumption(float64(vcpus), int(memory), hoursInMonth) / 1000
-
 		carbonImpact := calculateCarbonFootprint(powerConsumption, region)
+
+		references := instanceSpecs[string(instanceType.InstanceType)]
+		referenceList := ""
+		for _, ref := range references {
+			referenceList += ref.ResourceReference + ", "
+		}
+		if len(referenceList) > 0 {
+			referenceList = referenceList[:len(referenceList)-2]
+		}
 
 		table.Append([]string{
 			string(instanceType.InstanceType),
+			referenceList,
 			fmt.Sprintf("%d", vcpus),
 			fmt.Sprintf("%d", memory),
 			fmt.Sprintf("%.2f", powerConsumption),
